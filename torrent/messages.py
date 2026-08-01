cdd84a43125eed0f8101a6bb958354105a8e7365
Shaun07a <shaunjoseph004@gmail.com>
2026-08-01 22:27:31 +0530

    Updated the messages.py

--- torrent/messages.py
+++ torrent/messages.py
@@ -15,11 +15,14 @@ CANCEL = 8
 
 @dataclass
 class Message:
-    message_id: int
-    payload: bytes
 
-    def encode(self) -> bytes:
-        length = 1 + len(self.payload)
+    def __init__(self, message_id, payload=b""):
+        self.message_id = message_id
+        self.payload = payload
+
+    def encode(self):
+
+        length = len(self.payload) + 1
 
         return (
             struct.pack(">I", length)
@@ -28,10 +31,7 @@ class Message:
         )
 
     @classmethod
-    def decode(cls, data: bytes):
-
-        if len(data) < 5:
-            raise ValueError("Incomplete message")
+    def decode(cls, data):
 
         length = struct.unpack(">I", data[:4])[0]
 
@@ -39,10 +39,59 @@ class Message:
             return None
 
         message_id = data[4]
-        payload = data[5:4 + length]
 
-        return cls(message_id, payload)
+        payload = data[5:]
 
+        mapping = {
+            CHOKE: Choke,
+            UNCHOKE: Unchoke,
+            INTERESTED: Interested,
+            NOT_INTERESTED: NotInterested,
+            HAVE: Have,
+            BITFIELD: Bitfield,
+            REQUEST: Request,
+            PIECE: Piece,
+            CANCEL: Cancel,
+        }
+
+        message_class = mapping.get(message_id, Message)
+
+        return message_class(payload)
+
+
+class Choke(Message):
+    def __init__(self, payload=b""):
+        super().__init__(CHOKE, payload)
+
+
+class Unchoke(Message):
+    def __init__(self, payload=b""):
+        super().__init__(UNCHOKE, payload)
+
+
+class Interested(Message):
+    def __init__(self):
+        super().__init__(INTERESTED)
+
+
+class NotInterested(Message):
+    def __init__(self):
+        super().__init__(NOT_INTERESTED)
+
+
+class Have(Message):
+    def __init__(self, payload):
+        super().__init__(HAVE, payload)
+
+
+class Bitfield(Message):
+    def __init__(self, payload):
+        super().__init__(BITFIELD, payload)
+
+
+class Cancel(Message):
+    def __init__(self, payload):
+        super().__init__(CANCEL, payload)
 
 class Interested(Message):
     def __init__(self):
@@ -53,18 +102,28 @@ class Interested(Message):
 
 class Request(Message):
 
-    def __init__(self, index: int, begin: int, length: int):
+    def __init__(self, index, begin, length):
 
-        payload = (
-            struct.pack(">I", index)
-            + struct.pack(">I", begin)
-            + struct.pack(">I", length)
+        payload = struct.pack(
+            ">III",
+            index,
+            begin,
+            length
         )
 
-        super().__init__(
-            message_id=REQUEST,
-            payload=payload
-        )
+        super().__init__(REQUEST, payload)
+
+class Piece(Message):
+
+    def __init__(self, payload):
+
+        super().__init__(PIECE, payload)
+
+        self.index = struct.unpack(">I", payload[:4])[0]
+
+        self.begin = struct.unpack(">I", payload[4:8])[0]
+
+        self.block = payload[8:]
 
 
 MESSAGE_NAMES = {
